d8404a49847df6865abb150688b2f8f9c5facf8a
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-01-29 00:24:17 +0600

    remove null values

---
+++ src/com/roze/streams/filter/filterExample3/FilterExample3.java
@@ -0,0 +1,14 @@
+package com.roze.streams.filter.filterExample3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterExample3 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("One", "Two", null, "Three", null, "Four", "Five", null);
+        //removing the null values and print the other listed values
+        List<String> result = words.stream().filter(n -> n != null).collect(Collectors.toList());
+        System.out.println(result);
+    }
+}
