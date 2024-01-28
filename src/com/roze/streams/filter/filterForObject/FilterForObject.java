266076abe154482a01e15d79fa64a0eff1a3df48
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-01-29 00:36:54 +0600

    filter for object

---
+++ src/com/roze/streams/filter/filterForObject/FilterForObject.java
@@ -0,0 +1,17 @@
+package com.roze.streams.filter.filterForObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterForObject {
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList<>();
+        productsList.add(new Product(1L, "Apple Laptop", 150000));
+        productsList.add(new Product(2L, "Dell Laptop", 50000));
+        productsList.add(new Product(3L, "Sony Laptop", 120000));
+        productsList.add(new Product(4L, "Lenovo Laptop", 100000));
+        productsList.add(new Product(5L, "Asus Laptop", 40000));
+
+        productsList.stream().filter(n -> n.getPrice() > 50000).forEach(n -> System.out.println(n.toString()));
+    }
+}
