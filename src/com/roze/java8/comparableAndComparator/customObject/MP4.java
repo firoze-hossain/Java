11d81bfd8853c1f5d4d6cf4faa36f246a68ddea9
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-03 20:47:46 +0600

    use of sort for object

---
+++ src/com/roze/java8/comparableAndComparator/customObject/MP4.java
@@ -0,0 +1,20 @@
+package com.roze.java8.comparableAndComparator.customObject;
+
+import com.roze.java8.comparableAndComparator.genricMethod.util.Util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MP4 {
+    public static void main(String[] args) {
+        ArrayList<Song> musicList = new ArrayList<>();
+        musicList.add(new Song("Kabhi Albida", "Yami Gautam", 2013));
+        musicList.add(new Song("Holly Holly", "Sham Kapur", 2015));
+        musicList.add(new Song("New Poem", "Games Sine", 2009));
+        musicList.add(new Song("Noble", "Harbad ", 2000));
+        musicList.add(new Song("Tumi kobe esechile", "Manna Dey", 1995));
+        Util.iterateList(musicList);
+        //problem created
+        Collections.sort(musicList);
+    }
+}
---
+++ src/com/roze/java8/comparableAndComparator/customObject/Song.java
@@ -0,0 +1,46 @@
+package com.roze.java8.comparableAndComparator.customObject;
+
+public class Song {
+    private String title;
+    private String artist;
+    private Integer releaseDate;
+
+    public Song(String title, String artist, Integer releaseDate) {
+        this.title = title;
+        this.artist = artist;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Integer getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Integer releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", releaseDate=" + releaseDate +
+                '}';
+    }
+}
