b30ebd2cb93c4290d0bd8c8fae54bbfed5eb4d0e
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-09 22:08:12 +0600

    create objet with new keyword

--- src/com/roze/objectCreationIn5Ways/NewKeyword.java
+++ src/com/roze/objectCreationIn5Ways/NewKeyword.java
@@ -3,6 +3,7 @@ package com.roze.objectCreationIn5Ways;
 public class NewKeyword {
     public static void main(String[] args) {
         //create object with new keyword
+        //constructor invoke with new keyword
         Student s = new Student();
         //print object reference
         System.out.println(s);
