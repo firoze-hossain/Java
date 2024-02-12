a448e20c40c815378f61c7fcbb8ecc2c47b90161
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-12 19:38:13 +0600

    passed by values first example

---
+++ src/com/roze/interviewQAndAns/memoryManagement/ValuesAndReference.java
@@ -0,0 +1,15 @@
+package com.roze.interviewQAndAns.memoryManagement;
+
+public class ValuesAndReference {
+    public static void main(String[] args) {
+        int localValue = 5;
+        // java is always passed by copy of value
+        // java is not for passed by reference even for Objects(like-> String, Student etc)
+        calculate(localValue);
+        System.out.println(localValue);
+    }
+
+    public static void calculate(int calValue) {
+        calValue = calValue * 10;
+    }
+}
