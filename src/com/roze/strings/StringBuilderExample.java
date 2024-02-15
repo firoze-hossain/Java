f598b880ccd371ba2417ffd67f80056c9149c910
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-15 09:58:47 +0600

    StringBuilderExample with the performance with string

---
+++ src/com/roze/strings/StringBuilderExample.java
@@ -0,0 +1,16 @@
+package com.roze.strings;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            //it will not create the new object
+            //every iteration it will change the same object
+            //time complexity O(n)
+            //StringBuilder is mutable
+            stringBuilder.append(ch);
+        }
+        System.out.println(stringBuilder);
+    }
+}
