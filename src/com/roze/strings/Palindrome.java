04a62ed7ed6a7c3675af504013ea14b6dc9c8c6e
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-15 16:27:54 +0600

    string palindrome

---
+++ src/com/roze/strings/Palindrome.java
@@ -0,0 +1,37 @@
+package com.roze.strings;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        StringBuilder stringBuilder = new StringBuilder(str);
+        System.out.println(isPalindrome(stringBuilder.toString()));
+    }
+
+    //using StringBuilder
+    public static boolean isPalindrome(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        StringBuilder s = stringBuilder.reverse();
+        return str.equals(s.toString());
+    }
+
+    //using iteration and and conditional check
+    public static boolean isPalindrome1(String str) {
+        if (str == null || str.length() == 0) {
+            return true;
+        }
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length() / 2; i++) {
+            int start = str.charAt(i);
+            int end = str.charAt(str.length() - 1 - i);
+            if (start != end) {
+                return false;
+            }
+
+
+        }
+        return true;
+    }
+}
