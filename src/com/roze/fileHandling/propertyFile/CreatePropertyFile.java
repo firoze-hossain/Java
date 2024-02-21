5d5916af674425b47a09a28c1fec8f5572b2167c
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-02-21 18:11:27 +0600

    create properties file

--- src/com/roze/fileHandling/propertyFile/CreatePropertyFile.java
+++ src/com/roze/fileHandling/propertyFile/CreatePropertyFile.java
@@ -1,23 +1,26 @@
 package com.roze.fileHandling.propertyFile;
 
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class PropertyFile {
+public class CreatePropertyFile {
+
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.setProperty("Name", "Md. Firoze Hossain");
+        properties.setProperty("Name", "Firoze");
         properties.setProperty("Email", "firoze.hossain01@gmail.com");
-        properties.setProperty("Mobile", "01714271620");
-        String filePath = System.getProperty("user.dir") + "/data/hello.properties";
+        properties.setProperty("Age", "26");
+        String filePath = System.getProperty("user.dir") + "/data/tuk.properties";
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-            properties.store(fileOutputStream, "Hello Firoze");
+            properties.store(fileOutputStream, "Hello Java");
             fileOutputStream.close();
-            System.out.println("Properties has been written into: " + filePath);
+            System.out.println("Properties file created: " + filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
