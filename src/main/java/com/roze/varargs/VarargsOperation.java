c3bfac5e9e08f31857b39bb32fa773f7ecb36e37
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-03-16 23:28:59 +0600

    varargs as array with different array properties

--- src/main/java/com/roze/varargs/VarargsOperation.java
+++ src/main/java/com/roze/varargs/VarargsOperation.java
@@ -3,6 +3,11 @@ package com.roze.varargs;
 public class VarargsOperation {
     int sum = 0;
 
+    void m1(int... num) {
+        System.out.println("Length: " + num.length);
+        System.out.println("Element at Index(2): " + num[1]);
+    }
+
     int add(int... num) {
         for (int element : num) {
             sum += element;
@@ -12,11 +17,13 @@ public class VarargsOperation {
 
     public static void main(String[] args) {
         VarargsOperation operation = new VarargsOperation();
-        int[]num={10,1,3};
+        int[] num = {10, 1, 3};
 //        int resultByElement = operation.add(1, 10);
 //        System.out.println(resultByElement);
         System.out.println("***********");
-        int resultByArr=operation.add(num);
+        int resultByArr = operation.add(num);
         System.out.println(resultByArr);
+        System.out.println("Varargs as array");
+        operation.m1(1, 6, 3);
     }
 }
